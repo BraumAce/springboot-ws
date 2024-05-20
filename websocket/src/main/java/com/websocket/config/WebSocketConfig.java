00f7bd8ba3b87218f1803b14f6ba4504f0256efd
BraumAce <1693717911@qq.com>
2024-05-20 11:30:41 +0800

    upload WebSocket启动配置

--- websocket/src/main/java/com/websocket/config/WebSocketConfig.java
+++ websocket/src/main/java/com/websocket/config/WebSocketConfig.java
@@ -1,9 +1,13 @@
-package com.ws.websocketserver.config;
+package com.websocket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+/**
+ * WebSocketConfig 配置类
+ * 注入对象 ServerEndpointExporter
+ */
 @Configuration
 public class WebSocketConfig {
 
