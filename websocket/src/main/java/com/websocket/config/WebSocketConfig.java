0518c7c42757453a3ec25f16b4e8675414e0bdd5
BraumAce <1693717911@qq.com>
2024-05-20 17:03:07 +0800

    update 配置exchange交换机

--- websocket/src/main/java/com/websocket/config/WebSocketConfig.java
+++ websocket/src/main/java/com/websocket/config/WebSocketConfig.java
@@ -2,6 +2,10 @@ package com.websocket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
@@ -9,10 +13,39 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
  * 注入对象 ServerEndpointExporter
  */
 @Configuration
-public class WebSocketConfig {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//    @Bean
+//    public ServerEndpointExporter serverEndpointExporter() {
+//        return new ServerEndpointExporter();
+//    }
 
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
+    /**
+     * 注册STOMP端点
+     * @param registry
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        //客户端连接端点
+        registry.addEndpoint("/websocket")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
+
+    /**
+     * 配置消息交换机
+     * @param registry
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //registry.enableSimpleBroker("/topic/","/queue/");
+        registry.enableStompBrokerRelay("/topic/","/queue/","/exchange/")
+                .setRelayHost("localhost")
+                .setRelayPort(61613)
+                .setClientLogin("guest")
+                .setClientPasscode("guest")
+                .setVirtualHost("/");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
 }
