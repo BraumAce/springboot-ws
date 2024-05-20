c90ee40ca1230642d99a36a9a48e939158d76a02
BraumAce <1693717911@qq.com>
2024-05-20 11:22:58 +0800

    upload WebSocketConfig 配置类

---
+++ websocket-client-example/src/main/java/com/ws/websocketclient/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.ws.websocketclient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * WebSocketConfig 配置类
+ * 注入对象 ServerEndpointExporter
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
