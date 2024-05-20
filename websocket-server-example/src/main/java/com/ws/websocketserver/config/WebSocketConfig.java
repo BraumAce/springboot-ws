0c78d22b67b4d370c9aff6fc8eedced0d6da908f
BraumAce <1693717911@qq.com>
2024-05-20 11:25:01 +0800

    upload WebSocketConfig

---
+++ websocket-server-example/src/main/java/com/ws/websocketserver/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.ws.websocketserver.config;
+
+import com.ws.websocketserver.handler.MyWebSocketHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myWebSocketHandler(), "/ws").setAllowedOrigins("*");
+    }
+
+    @Bean
+    public WebSocketHandler myWebSocketHandler() {
+        return new MyWebSocketHandler();
+    }
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
