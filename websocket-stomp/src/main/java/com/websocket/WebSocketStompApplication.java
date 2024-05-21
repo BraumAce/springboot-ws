9ed2ea71b3f911fbcb98c0d8f50e0446202148a4
BraumAce <1693717911@qq.com>
2024-05-21 13:26:06 +0800

    upload 启动类

---
+++ websocket-stomp/src/main/java/com/websocket/WebSocketStompApplication.java
@@ -0,0 +1,12 @@
+package com.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketStompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketStompApplication.class, args);
+    }
+}
