579cc2d997bcb66174c57452c97243ba55ff0d67
BraumAce <1693717911@qq.com>
2024-05-22 22:49:32 +0800

    update 消息内容格式

--- websocket-stomp/src/main/java/com/websocket/controller/WebSocketController.java
+++ websocket-stomp/src/main/java/com/websocket/controller/WebSocketController.java
@@ -1,10 +1,7 @@
 package com.websocket.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -28,18 +25,20 @@ public class WebSocketController {
     public void sendToUser(Map<String, String> params) {
         String fromUserId = params.get("fromUserId");
         String toUserId = params.get("toUserId");
-        String msg = "来自用户" + fromUserId + "消息：" + params.get("msg");
+        String msg = "来自用户" + fromUserId + "的消息：" + params.get("msg");
         String destination = "/topic/user" + toUserId;
         template.convertAndSend(destination, msg);
     }
 
     /**
      * 群发消息
-     * @param msg 消息内容
+     * @param params 消息参数
      */
     @MessageMapping("/sendToAll")
-    public void sendToAll(String msg) {
-        String destination = "/queue/chat";
+    public void sendToAll(Map<String, String> params) {
+        String fromUserId = params.get("fromUserId");
+        String msg = "来自用户" + fromUserId + "的广播：" + params.get("msg");
+        String destination = "/topic/chat";
         template.convertAndSend(destination, msg);
     }
 
